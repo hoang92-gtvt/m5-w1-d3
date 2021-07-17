b613f80f53e0f69ddfc148c8cc357b2ecab16cb2
hoang-gtvt <hoang92.gtvt@gmail.com>
2021-07-17 10:34:26 +0700

    chuyen component qua rout, nhap tren url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {RoutingComponent} from "./routing/routing.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'home', component:HomeComponent},
+  {path:'rout', component:RoutingComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyformComponent } from './myform/myform.component';
+import { RoutingComponent } from './routing/routing.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyformComponent
+    MyformComponent,
+    RoutingComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
